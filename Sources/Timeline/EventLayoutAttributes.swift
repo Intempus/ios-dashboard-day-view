fecb98bd4d2d5b85051b7ff9b9772ad471358480
Sakshi patil <sakshi.patil@visma.com>
2024-10-09 11:07:31 +0200

    - set minimum event height of event so it displays the details in appropriate space.

--- Sources/Timeline/EventLayoutAttributes.swift
+++ Sources/Timeline/EventLayoutAttributes.swift
@@ -2,7 +2,13 @@ import CoreGraphics
 
 public final class EventLayoutAttributes {
     public let descriptor: EventDescriptor
-    public var frame = CGRect.zero
+    public var frame = CGRect.zero {
+        didSet {
+            if frame.height < 35.0 {
+                frame.size.height = 35.0
+            }
+        }
+    }
     
     public init(_ descriptor: EventDescriptor) {
         self.descriptor = descriptor
